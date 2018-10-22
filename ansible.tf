51038935a0347567ee5f79766cab517b72b316b2
DanRoz <danroz3@gmail.com>
2018-10-22 15:25:26 -0700

    Remove the GIF copy in the playbook

--- ansible.tf
+++ ansible.tf
@@ -32,14 +32,17 @@ resource "aws_s3_bucket_object" "PutFile" {
 
 
 resource "aws_instance" "nginx" {
- ami = "ami-0f65671a86f061fcd"
- instance_type = "t2.micro"
- key_name = "${var.key_name}"
+ ami               = "ami-0f65671a86f061fcd"
+ instance_type     = "t2.micro"
+ key_name          = "${var.key_name}"
+ count             = 2
+ availability_zone = "us-east-2a"
 
  connection {
   user = "ubuntu"
   private_key = "${file(var.private_key_path)}"
  }
+
  provisioner "file" {
   source      = "/home/ubuntu/AnsibleTerraform/install-nginx.yaml"
   destination = "/tmp/install-nginx.yaml"
@@ -51,14 +54,28 @@ resource "aws_instance" "nginx" {
    "sudo apt-get update",
    "sudo apt-get install ansible -y",
    "sudo ansible-playbook /tmp/install-nginx.yaml"
-   
   ]
+
  }
+}
+
+resource "aws_elb" "myelb" {
+ name = "nginx-tf-elb"
+ availability_zones = ["us-east-2a"]
+ 
+ listener {
+  instance_port     = 80
+  instance_protocol = "http"
+  lb_port           = 80
+  lb_protocol       = "http"
+ }
+ 
+ instances = ["${aws_instance.nginx.*.id}"]
 }
 
 #OUTPUT
 
-output "aws_instance_public_dns" {
- value = "${aws_instance.nginx.public_dns}"
+output "aws_elb_public_dns" {
+ value = "${aws_elb.myelb.dns_name}"
 }
 
